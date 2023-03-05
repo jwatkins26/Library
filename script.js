ec2a74cdaa2ec681513e2aafe0b126e70fbcae2e
John Watkins <johnwes87@protonmail.com>
2023-03-05 11:16:10 -0600

    Book Constructor

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
+
 function openForm() {
     document.getElementById('addForm').style.display = 'block'
 }
 
 function closeForm() {
     document.getElementsByClassName('cancel').style.display = 'none'
+}
+
+// Library
+let myLibrary = []
+
+// Book 
+function Book(name, author, pages, read) {
+    this.name = name
+    this.author = author
+    this.pages = pages
+    this. read = read
 }
