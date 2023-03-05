7470c273808cba564626366da52b2a145946818c
John Watkins <johnwes87@protonmail.com>
2023-03-05 15:17:09 -0600

    add book to library

--- script.js
+++ script.js
@@ -1,19 +1,39 @@
+let newBook = document.querySelector('#newBook');
+let cancel = document.querySelector('.cancel');
 
-function openForm() {
-    document.getElementById('addForm').style.display = 'block'
-}
+newBook.addEventListener('click', function() {
+    document.getElementById('addForm').style.display = 'block';
+})
 
-function closeForm() {
-    document.getElementsByClassName('cancel').style.display = 'none'
-}
+cancel.addEventListener('click', function() {
+    document.getElementById('addForm').style.display = 'none';
+})
 
 // Library
 let myLibrary = []
 
 // Book 
-function Book(name, author, pages, read) {
-    this.name = name
+function Book(title, author, pages, read) {
+    this.title = title
     this.author = author
     this.pages = pages
     this.read = read
 }
+
+// Add Book
+
+
+function addBook() {
+    let title = document.querySelector('#title').value;
+    let author = document.querySelector('#author').value;
+    let pages = document.querySelector('#pages').value;
+    let read = document.querySelector('#read').checked;
+    let addedBook = new Book(title, author, pages, read);
+    myLibrary.push(addedBook);
+    console.log(addedBook);
+}
+
+document.querySelector('#bookForm').addEventListener('submit', function(event){
+    event.preventDefault();
+    addBook();
+})
